d3127b108b2778f6cf0beffa07a96a04537fc22a
Jorge Antonio Silva Cortés <45528024+Joansico97@users.noreply.github.com>
2025-05-07 16:25:39 -0500

    add: changes for sms

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,29 +1,26 @@
-import Flutter
 import UIKit
-import flutter_local_notifications
-import FirebaseCore
+import Flutter
+import Firebase
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    // 1. Configura Firebase (PRIMER PASO)
     FirebaseApp.configure()
-    
-    // 2. Configuración de notificaciones locales
-    FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in 
-      GeneratedPluginRegistrant.register(with: registry)
-    }
-      
-    if #available(iOS 10.0, *) {
-      UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
-    }
-    
-    // 3. Registro de plugins estándar
     GeneratedPluginRegistrant.register(with: self)
-    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  override func application(
+    _ app: UIApplication,
+    open url: URL,
+    options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+  ) -> Bool {
+    if Auth.auth().canHandle(url) {
+      return true
+    }
+    return super.application(app, open: url, options: options)
   }
 }
