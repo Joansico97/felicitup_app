1f0ffdf0607e12b80cd3ecdc2683fc84a0e9ffb8
Jorge Antonio Silva Cortés <45528024+Joansico97@users.noreply.github.com>
2025-07-21 13:12:51 -0500

    add: remove unsed import

--- lib/features/auth/federated_register/views/form_federated_view.dart
+++ lib/features/auth/federated_register/views/form_federated_view.dart
@@ -9,7 +9,6 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-import 'package:intl/intl.dart';
 
 class FormFederatedView extends StatefulWidget {
   const FormFederatedView({super.key});
